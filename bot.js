49130a88775177d38a4a35ade6b66f191d5c81cf
mertbabspro <142105576+mertbabspro@users.noreply.github.com>
2026-03-01 11:35:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ function createBot() {
   const bot = mineflayer.createBot({
     host: 'zurnacraft.net',
     port: 25565,
-    username: 'Fevri03', // Sabit isim
+    username: 'ytufafk', // Sabit isim
     version: '1.21.4',
     auth: 'offline',
     hideErrors: false
@@ -143,3 +143,4 @@ function createBot() {
 }
 
 createBot()
+
